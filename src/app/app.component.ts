5aad30ad898563bcbdb038ee394188d56ce99aec
Piotr <murawskip00@gmail.com>
2023-07-27 17:59:10 +0200

    Random commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'currency-rate-app-ng';
+
+  fromCurrency = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(3)])
+  requesterName = new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(32)])
+
+  constructor(private http: HttpClient) {
+  }
+
+  calculateCurrencyRate() {
+    if (this.fromCurrency.valid && this.requesterName.valid) {
+      const apiUrl = "localhost:8080/currencies/get-current-currency-value-command";
+
+      const body = {
+        currency: this.fromCurrency.value,
+        name: this.requesterName.value
+      };
+      this.http.post(apiUrl, body).subscribe((data: any) => {
+        console.log(`Rate from ${this.fromCurrency.value}: ${data.rate}`);
+      }, error => {
+        console.error("There was an error!", error)
+      });
+    } else {
+      console.error("Invalid input date!");
+    }
+  }
 }
